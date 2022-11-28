d87eecc2a16346d63d239ff24118461b9051ff8f
Eric Wich <eric@wich.tech>
2022-11-28 15:52:35 -0500

    Weekly turnover

---
+++ src/data-2.js
@@ -0,0 +1,51 @@
+export const week = '2022-10-24';
+export default [
+  {
+    category: 'VC',
+    words: [
+      'get',
+      'swim',
+      'run',
+      'sit',
+      'shut',
+      'skip',
+    ],
+    description: 'Vowel followed by a consonent',
+  },
+  {
+    category: 'Double',
+    words: [
+      'getting',
+      'swimming',
+      'running',
+      'sitting',
+      'shutting',
+      'skipping',
+    ],
+    description: 'Word with a double consonent pair in the middle',
+  },
+  {
+    category: 'VCC',
+    words: [
+      'rest',
+      'yell',
+      'pick',
+      'stand',
+      'pass',
+      'jump',
+    ],
+    description: 'Vowel followed by a consonent',
+  },
+  {
+    category: 'Nothing',
+    words: [
+      'resting',
+      'yelling',
+      'picking',
+      'standing',
+      'passing',
+      'jumping',
+    ],
+    description: 'No special rules',
+  },
+];
